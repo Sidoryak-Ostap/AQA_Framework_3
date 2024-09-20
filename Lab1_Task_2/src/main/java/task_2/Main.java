18c9e7f33cddcbac4c0d610467fa8e5a41e3a043
Sidoryak-Ostap <ostapsidorak@gmail.com>
2024-09-20 15:21:21 +0300

    task_8

--- Lab1_Task_2/src/main/java/task_2/Main.java
+++ Lab1_Task_2/src/main/java/task_2/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args){
 
-
-
         Day day = Day.FRIDAY;
-
-
         switch (day){
             case  day.SUNDAY: {
                 System.out.println("Today is Sunday");
